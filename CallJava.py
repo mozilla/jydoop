cb18d8598dde3294b85d3aff2a5108c4be6067e8
Taras Glek <tglek@mozilla.com>
2013-03-06 12:02:18 -0800

    got wordcount to run partially in python

--- CallJava.py
+++ CallJava.py
@@ -1,2 +1,8 @@
 def callme(out):
     out.println("from python")
+
+def map(value, one, outword, context):
+    for f in value.toString().split(" "):
+        outword.set(f)
+        context.write(outword, one);
+        
---
+++ WordCount.java
@@ -0,0 +1,85 @@
+package org.apache.hadoop.examples;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.python.core.PyObject;
+import org.python.util.PythonInterpreter;
+
+public class WordCount {
+  private static PythonInterpreter py = null;
+  
+  public static void Python(String methodName, Object arg1, Object arg2, Object arg3, Object arg4) {
+    if (py == null) {
+      py = new PythonInterpreter();
+      py.exec("import CallJava");
+    }
+    py.set("a1", arg1);
+    if (arg2 == null)
+      py.exec("CallJava." + methodName + "(a1)");
+    else {
+      py.set("a2", arg2);
+      py.set("a3", arg3);
+      py.set("a4", arg4);
+      py.exec("CallJava." + methodName + "(a1, a2, a3, a4)");
+    }
+  }
+  
+  public static class TokenizerMapper 
+       extends Mapper<Object, Text, Text, IntWritable> {
+    
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+      
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      Python("map", value, one, word, context);
+
+    }
+  }
+  
+  public static class IntSumReducer 
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values, 
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: wordcount <in> <out>");
+      System.exit(2);
+    }
+    Job job = new Job(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
