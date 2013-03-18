0e5e31bb1655041d6c3600b82de92a3971713efc
Taras Glek <taras@mozilla.com>
2013-03-18 08:06:20 -0700

    pass-through map-only jobs

--- FileDriver.py
+++ FileDriver.py
@@ -1,8 +1,14 @@
+import sys
+
 class LocalContext:
-    def __init__(self):
+    def __init__(self, map_only = False):
         self.result = {}
+        self.map_only = map_only
 
     def write(self, key, value):
+        if self.map_only:
+            print "%s %s" % (key, value)
+            return
         try:
             self.result[key].append(value)
         except KeyError:
@@ -14,17 +20,27 @@ class LocalContext:
                 print "%s %s" % (key, v)
 
 def map_reduce(module, file):
-    context = LocalContext()
-    f = open(file)
-    for l in f.readlines():
-        module.map(str(f.tell()), l, context)
-    f.close()
     try:
         reducefunc = module.reduce
     except AttributeError:
         reducefunc = None
-        context.dump()
-        return
+
+    context = LocalContext(reducefunc == None)
+
+    if file == "-":
+        f = sys.stdin
+    else:
+        f = open(file)
+    total = 0;
+    while True:
+        l = f.readline()
+        length = len(l)
+        total += length
+        if length == 0:
+            break
+        module.map(str(total), l, context)
+    f.close()
+
     reduced_context = LocalContext()
     for key, values in context.result.iteritems():
         module.reduce(key, values, reduced_context)
