b6f28a38ffde23544f014c984c03e1fd5f1b76de
Benjamin Smedberg <benjamin@smedbergs.us>
2013-03-30 06:16:57 -0700

    Add a sampler to osdistribution for faster testing.

--- scripts/osdistribution.py
+++ scripts/osdistribution.py
@@ -1,8 +1,11 @@
 # mapreduce job to count the distribution of OS data
 
 import json
+import random
 
 def map(k, v, cx):
+    if random.random() > 0.05:
+        return
     j = json.loads(v)
     os = j['info']['OS']
     cx.write(os, 1)
