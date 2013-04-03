b0620a7b8281723febeba5f445f7879a76579736
Benjamin Smedberg <benjamin@smedbergs.us>
2013-04-03 16:34:05 -0700

    Make osdistribution use the combiner framework, and document it liberally.

--- scripts/osdistribution.py
+++ scripts/osdistribution.py
@@ -10,5 +10,23 @@ def map(k, v, cx):
     os = j['info']['OS']
     cx.write(os, 1)
 
+def combine(k, vlist, cx):
+    """
+    Combining happens on each map node, and is called with an arbitrary number
+    map records which have the same key. The combiner, if present, should output a record
+    of the same "schema" as the map function does.
+
+    In this case, we combine the records by counting the records.
+    """
+    cx.write(k, sum(vlist))
+
 def reduce(k, vlist, cx):
+    """
+    After all of the map records for a particular key have been collected, reduction
+    can count or combine these records (it could e.g. produce a histogram or some other
+    complex structure).
+
+    In this case, reduction is the exact same as combining, it just sums the map and/or
+    combine results together.
+    """
     cx.write(k, sum(vlist))
