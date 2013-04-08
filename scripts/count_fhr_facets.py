9326ca828a046d63d6dc15742c2de2c50ff428ca
Mark Reid <mreid@mozilla.com>
2013-04-08 16:33:43 -0300

    Add an example to analyze Health Report data.

---
+++ scripts/count_fhr_facets.py
@@ -0,0 +1,31 @@
+import json
+import pydoop
+import healthreportutils
+
+setupjob = healthreportutils.setupjob
+
+combine = pydoop.sumreducer
+
+def map(key, value, context):
+    try:
+        payload = json.loads(value)
+    except:
+        context.write("Bogus\tBogus\tBogus\tBogus", 1)
+        return
+
+    output = []
+    try:
+        info = payload['geckoAppInfo']
+        if type(info) == dict:
+            output.append(info['name'])
+            output.append(info['os'])
+            output.append(info['updateChannel'])
+            output.append(info['version'])
+    except KeyError:
+        pass
+
+    if len(output) == 4:
+        outkey = "\t".join(output)
+        context.write(outkey, 1)
+
+reduce = pydoop.sumreducer
