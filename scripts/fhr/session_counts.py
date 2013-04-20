c8d737cfef56d1f39615b7346b2a874515f81a94
Gregory Szorc <gps@mozilla.com>
2013-04-20 12:39:16 -0700

    Script to retrieve FHR session counts

---
+++ scripts/fhr/session_counts.py
@@ -0,0 +1,33 @@
+# Copyright 2013 The Mozilla Foundation <http://www.mozilla.org/>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import jydoop
+
+from healthreportutils import (
+    FHRMapper,
+    setupjob,
+)
+
+
+@FHRMapper(only_major_channels=True)
+def map(key, payload, context):
+    channel = payload.channel
+
+    for day, session in payload.session_times():
+        state = 'clean' if session.clean else 'aborted'
+        context.write('\t'.join((channel, day, state)), 1)
+        context.write('\t'.join((channel, day, 'total')), 1)
+
+
+combine = reduce = jydoop.sumreducer
