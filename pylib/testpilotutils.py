1b1eb6e92c5dc52561023d0d42707502577ccae1
Mark Reid <mreid@mozilla.com>
2013-06-17 15:54:14 -0300

    Iterate properly from Start Date to End Date.

--- pylib/testpilotutils.py
+++ pylib/testpilotutils.py
@@ -13,32 +13,40 @@ def setupjob(job, args):
     """
 
     import java.text.SimpleDateFormat as SimpleDateFormat
+    import java.util.Date as Date
     import java.util.Calendar as Calendar
+    import com.mozilla.util.DateUtil as DateUtil
+    import com.mozilla.util.DateIterator as DateIterator
     import org.apache.hadoop.mapreduce.lib.input.FileInputFormat as FileInputFormat
     import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsTextInputFormat as MyInputFormat
 
     if len(args) != 3:
         raise Exception("Usage: <testpilot_study> <startdate-YYYY-MM-DD> <enddate-YYYY-MM-DD>")
 
+    # use to collect up each date in the given range
+    class MyDateIterator(DateIterator):
+       def __init__(self):
+          self._list = []
+       def get(self):
+          return self._list
+       def see(self, aTime):
+          self._list.append(aTime)
+
+    sdf = SimpleDateFormat(dateformat)
     study = args[0]
-    startdate = args[1]
-    enddate = args[2]
+    startdate = Calendar.getInstance()
+    startdate.setTime(sdf.parse(args[1]))
+
+    enddate = Calendar.getInstance()
+    enddate.setTime(sdf.parse(args[2]))
+
+    dates = MyDateIterator()
+
+    DateUtil.iterateByDay(startdate.getTimeInMillis(), enddate.getTimeInMillis(), dates)
+
+    paths = []
+    for d in dates.get():
+       paths.append(pathformat % (study, sdf.format(Date(d))))
 
     job.setInputFormatClass(MyInputFormat)
-
-    # TODO: iterate properly
-    paths = [pathformat % (study, startdate), pathformat % (study, enddate)]
-    
-    #    final List<String> paths = new ArrayList<String>();
-    #    final String finalLocation = location;
-    #    DateUtil.iterateByDay(start.getTimeInMillis(), end.getTimeInMillis(), new DateIterator(){
-    #        @Override
-    #        public void see(long aTime) {
-    #            String aDate = inputPathFormat.format(new Date(aTime));
-    #            String path = finalLocation.replace("%DATE%", aDate);
-    #            LOG.info("Adding an input location: '" + path + "'");
-    #            paths.add(path);
-    #        }
-    #    });
-    #    
     FileInputFormat.setInputPaths(job, ",".join(paths));
