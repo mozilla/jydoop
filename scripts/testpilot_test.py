c31a9c94df59a1091e3ef9a3035d7ce532db55ed
Mark Reid <mreid@mozilla.com>
2013-06-17 14:23:57 -0300

    Add a basic testpilot test

---
+++ scripts/testpilot_test.py
@@ -0,0 +1,24 @@
+try:
+    import simplejson as json
+    print "Using simplejson for faster json parsing"
+except ImportError:
+    import json
+import testpilotutils
+import jydoop
+import sys
+
+def map(key, value, context):
+    try:
+        payload = json.loads(value)
+        if "personid" in payload:
+           context.write(str(payload["personid"]), 1)
+        else:
+           context.write("noperson", 1)
+    except:
+        context.write("error", 1)
+
+combine = jydoop.sumreducer
+
+reduce = jydoop.sumreducer
+
+setupjob = testpilotutils.setupjob
