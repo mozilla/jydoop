c7ae599e685fe6f08363b70e96a1d6184bc208ca
Mark Reid <mreid@mozilla.com>
2013-08-02 15:54:47 -0300

    Add a basic setupjob function for raw seqfiles

---
+++ pylib/sequencefileutils.py
@@ -0,0 +1,18 @@
+"""Utilities for querying generic text-based sequence files using jydoop."""
+
+def setupjob(job, args):
+    """
+    Set up a job to run on one or more HDFS locations
+
+    Jobs expect one or more arguments, the HDFS path(s) to the data.
+    """
+
+    import org.apache.hadoop.mapreduce.lib.input.FileInputFormat as FileInputFormat
+    import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsTextInputFormat as MyInputFormat
+
+    if len(args) < 1:
+        raise Exception("Usage: <hdfs-location1> [ <location2> ] [ <location3> ] [ ... ]")
+
+    job.setInputFormatClass(MyInputFormat)
+    FileInputFormat.setInputPaths(job, ",".join(args));
+    job.getConfiguration().set("org.mozilla.jydoop.mappertype", "TEXT")
